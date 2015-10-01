5395b9bcb41474eee767319494b69af41c0c24c8
Jan Beich <jbeich@FreeBSD.org>
2015-10-01 13:37:31 +0000

    Always use POSIX header
    In file included from ExternalLibs/ttvfs/VFSTools.cpp:18:
    /usr/include/sys/dir.h:39:2: warning: "The information in this file should be obtained from <dirent.h>" [-W#warnings]
    #warning "The information in this file should be obtained from <dirent.h>"
     ^
    /usr/include/sys/dir.h:40:2: warning: "and is provided solely (and temporarily) for backward compatibility." [-W#warnings]
    #warning "and is provided solely (and temporarily) for backward compatibility."
     ^
    2 warnings generated.

--- ExternalLibs/ttvfs/VFSTools.cpp
+++ ExternalLibs/ttvfs/VFSTools.cpp
@@ -12,11 +12,7 @@
 #   include <windows.h>
 #   include <io.h>
 #else
-#  ifdef __HAIKU__
-#    include <dirent.h>
-#  else
-#    include <sys/dir.h>
-#  endif
+#  include <dirent.h>
 #  include <unistd.h>
 #endif
 
